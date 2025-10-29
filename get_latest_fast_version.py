3b9ce188385409f12c2963c98ed03bfa9fa3cf52
Erik Smistad <ersmistad@gmail.com>
2025-10-29 11:51:55 +0100

    Add message to RuntimeError in get_latest_fast_version.py

--- get_latest_fast_version.py
+++ get_latest_fast_version.py
@@ -5,7 +5,7 @@ def get_latest_version():
     if response.status_code == 200:
         return response.json()['name'][1:]
     else:
-        raise RuntimeError()
+        raise RuntimeError(str(response.status_code) + ': ' + response.text)
 
 if __name__ == '__main__':
     print(get_latest_version())
